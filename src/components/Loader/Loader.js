275a9442021cb01eda108425002d3460983b04af
Muhammed Abdrahman <muhammed03amin.com>
2022-06-16 13:07:41 +0600

    feat: create loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function Loader() {
+  return (
+    <div className="loadingio-spinner-rolling-0568z3svhyex">
+      <div className="ldio-vmdjtxokxx">
+        <div />
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
---
+++ src/components/MiniLoader/MiniLoader.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function MiniLoader() {
+  return (
+    <div className="loadingio-spinner-rolling-me9qr9zffnk">
+      <div className="ldio-qnm5fi8q27p">
+        <div />
+      </div>
+    </div>
+  );
+}
+
+export default MiniLoader;
