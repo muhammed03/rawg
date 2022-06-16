964b260ecbba89703e2298f5fd01d852f05001f0
Muhammed Abdrahman <muhammed03amin.com>
2022-06-16 13:08:53 +0600

    feat: add service to get data

---
+++ src/services/rawg-service.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const baseUrl = 'https://api.rawg.io/api';
+const apiKey = '378d5bca1f484b1a9628790bba7a670c';
+
+const getGamesList = async (filterParams) => {
+  const params = new URLSearchParams();
+  for (const [key, value] of Object.entries(filterParams)) {
+    if (value) {
+      params.append(key, value);
+    }
+  }
+
+  const res = await axios.get(`${baseUrl}/games?key=${apiKey}&${params.toString()}`);
+
+  return res.data.results;
+};
+
+export { getGamesList };
